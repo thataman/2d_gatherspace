ef7544f834a332d9478d472db840447bcf6e789a
Aman raj <amanyuvraj2004@gmail.com>
2024-12-29 23:25:19 +0530

    frontend part

---
+++ 2D_GATHERSPACE/apps/frontend/src/App.tsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div className="bg-slate-600 rounded-sm">App</div>
+  )
+}
+
+export default App
---
+++ 2D_GATHERSPACE/apps/frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+export default  {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include TypeScript and JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
