c41128ab3b3adfaad3359865425fa0caa82816a1
Aman raj <amanyuvraj2004@gmail.com>
2025-01-04 16:10:09 +0530

    chnages in websocket

--- 2D_GATHERSPACE/apps/ws/src/index.ts
+++ 2D_GATHERSPACE/apps/ws/src/index.ts
@@ -1,13 +1,23 @@
 import { WebSocketServer } from 'ws';
-
+import { User } from './User';
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
+    let user = new User(ws)
   ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
 
-  ws.send('something');
+  const initialdelay = setTimeout(()=>{
+    const payload = {
+      type : "connected"
+       }
+       ws.send(JSON.stringify(payload))
+  },1000)
+ 
+
+  ws.on('close', function message(data) {
+    user.destroy()
+    clearTimeout(initialdelay)
+ });
+  
 });
