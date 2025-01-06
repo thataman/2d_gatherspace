3148f24a4fff076e2f4230c7d5591c3ffaaa13e1
Aman raj <amanyuvraj2004@gmail.com>
2025-01-06 22:59:09 +0530

    admin route

--- 2D_GATHERSPACE/apps/http/src/routes/adminroute.ts
+++ 2D_GATHERSPACE/apps/http/src/routes/adminroute.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { addElement,updateElement,createMap,createAvatars } from "../controllers/adminController";
+import { createElement,updateElement,createMap,createAvatar } from "../controllers/adminController";
 import { adminMiddleware } from "../middleware/adminmiddleware";
 export const adminRouter = Router()
 
-adminRouter.route("/element").post(adminMiddleware,addElement)
-adminRouter.route("/element/:elementid").put(adminMiddleware,updateElement)
+adminRouter.route("/element").post(adminMiddleware,createElement)
+adminRouter.route("/element/:elementId").put(adminMiddleware,updateElement)
 adminRouter.route("/map").post(adminMiddleware,createMap)
-adminRouter.route("/avatar").post(adminMiddleware,createAvatars)
+adminRouter.route("/avatar").post(adminMiddleware,createAvatar)
