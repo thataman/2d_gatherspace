4963712b64e9683a9540cd1663831b8bd05802ec
Aman raj <amanyuvraj2004@gmail.com>
2025-01-09 23:00:00 +0530

    element data

--- 2D_GATHERSPACE/apps/frontend/game/elementslist.tsx
+++ 2D_GATHERSPACE/apps/frontend/game/elementslist.tsx
@@ -1,19 +1,25 @@
-
-
 const Elementslist =[
     {
 key : "apple",
-sprite : "./trees/apple.png"
+sprite : "./trees/apple.png",
+width :16,
+height : 16,
+static : true
     },
     {
 key : "deadtree",
-sprite : "./trees/dead tree.png"
+sprite : "./trees/dead tree.png",
+width :16,
+height : 16,
+static : true
     },
     { 
 key : "grass",
-sprite : "./trees/Grass Block 2.png"
+sprite : "./trees/Grass Block 2.png",
+width :16,
+height : 16,
+static : true
     }
 ]
-
 
 export default Elementslist
